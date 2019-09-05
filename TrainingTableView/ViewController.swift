db9057bb2bed67aa25591d2f7383a20d01fef887
Le Thanh <thanhln@tinhvan.com>
2019-09-05 11:13:07 +0700

    initial commit

---
+++ TrainingTableView/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TrainingTableView
+//
+//  Created by mac on 9/5/19.
+//  Copyright © 2019 mac. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
